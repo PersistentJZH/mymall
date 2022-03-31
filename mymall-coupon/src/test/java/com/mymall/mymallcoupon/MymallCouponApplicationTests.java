015caa9870ea8157094b13529ea488e4a40aac04
jianzhihao <1454141926@qq.com>
2022-03-31 16:35:12 +0800

    项目初始结构

---
+++ mymall-coupon/src/test/java/com/mymall/mymallcoupon/MymallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymall.mymallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mymall-product/src/test/java/com/mymall/mymallproduct/MymallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymall.mymallproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
